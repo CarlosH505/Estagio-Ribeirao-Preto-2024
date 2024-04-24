0d41a2552082cfa5573a067528d87a6f501b5b1c
Carlos Henrique <carlinhoshenrique505@gmail.com>
2024-04-24 00:03:29 -0300

    Questao 4

---
+++ src/Questao4.java
@@ -0,0 +1,3 @@
+/*
+Ligo o primeiro interruptor e espero alguns minutos. Desligo o primeiro interruptor e ligo o segundo. Vou até a primeira lâmpada e verifico. Caso a lâmpada esteja apagada e quente, o interruptor correto é o número 1, pois isso indica que ela estava ligada. Caso a lâmpada esteja acesa, o interruptor correto é o 2, pois foi este que estava ligado. Caso a lâmpada esteja apagada e fria, o interruptor correto é o 3, pois isso indica que ela não foi ligada. Depois, iria até a segunda lâmpada e verificaria se ela se enquadra nos critérios acima e, já sabendo a situação da primeira, iria definir as demais. Após identificar a segunda, o terceiro interruptor seria o restante.
+*/
